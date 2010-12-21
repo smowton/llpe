af15da936676f8d359ad6df9e853fa98af39ed06
Chris Smowton <cs448@cam.ac.uk>
2010-12-21 18:35:15 +0000

    Remove pthreads and modify Makefile to do two extra opt passes.
    Both these passes contribute significantly: the first gives certainty about the first call to __wrap_read, and the second gives certainty about *every* call.
    
    The end result program is optimal except for default opt's unwillingness to unroll the main loop or to use special knowledge about memcpy and therefore eliminate it entirely.

--- fake_fd_table.c
+++ fake_fd_table.c
@@ -3,7 +3,7 @@
 
 #include <pthread.h>
 
-pthread_mutex_t fake_fd_mutex = PTHREAD_MUTEX_INITIALIZER;
+//pthread_mutex_t fake_fd_mutex = PTHREAD_MUTEX_INITIALIZER;
 #define FAKE_FD_OFFSET 0x40000000
 int last_fake_fd = 0x40000000;
 struct fake_fd fake_fd_array[1024];
@@ -13,14 +13,14 @@ __attribute__((always_inline)) struct fake_fd* new_fake_fd(int* new_fd) {
 
   struct fake_fd* ret;
 
-  pthread_mutex_lock(&fake_fd_mutex);
+  //  pthread_mutex_lock(&fake_fd_mutex);
 
   *new_fd = last_fake_fd++;
   int offset = (*new_fd) - FAKE_FD_OFFSET;
   ret = &fake_fd_array[offset];
   fake_fd_taken[offset] = 1;
 
-  pthread_mutex_unlock(&fake_fd_mutex);
+  //  pthread_mutex_unlock(&fake_fd_mutex);
 
   return ret;
 
@@ -30,7 +30,7 @@ __attribute__((always_inline)) struct fake_fd* get_fake_fd(int fd){
 
   struct fake_fd* ret;
 
-  pthread_mutex_lock(&fake_fd_mutex);
+  //  pthread_mutex_lock(&fake_fd_mutex);
 
   int offset = fd - FAKE_FD_OFFSET;
   if(!fake_fd_taken[offset])
@@ -38,7 +38,7 @@ __attribute__((always_inline)) struct fake_fd* get_fake_fd(int fd){
   else
     ret = &(fake_fd_array[offset]);
 
-  pthread_mutex_unlock(&fake_fd_mutex);
+  //  pthread_mutex_unlock(&fake_fd_mutex);
 
   return ret;
 
@@ -46,11 +46,11 @@ __attribute__((always_inline)) struct fake_fd* get_fake_fd(int fd){
 
 __attribute__((always_inline)) void delete_fake_fd(int fd) {
 
-  pthread_mutex_lock(&fake_fd_mutex);
+  //  pthread_mutex_lock(&fake_fd_mutex);
 
   fake_fd_taken[fd - FAKE_FD_OFFSET] = 0;
 
-  pthread_mutex_unlock(&fake_fd_mutex);
+  //  pthread_mutex_unlock(&fake_fd_mutex);
 
 }
 
--- test/test-bufsizes.py
+++ test/test-bufsizes.py
@@ -7,14 +7,14 @@ next_test = 1
 while next_test <= 1024:
     with open("bufsize.h", "w") as f:
         f.write("#define BUFSIZE %d" % next_test)
-    subprocess.Popen("make wc-plain", shell=True).wait()
+#    subprocess.Popen("make wc-plain", shell=True).wait()
     subprocess.Popen("make wc-test", shell=True).wait()
-    subprocess.Popen("./wc-plain").wait()
-    for i in range(5):
-        start_time = datetime.now()
-        subprocess.Popen("./wc-plain").wait()
-        elapsed = datetime.now() - start_time
-        print next_test, "plain", elapsed
+#    subprocess.Popen("./wc-plain").wait()
+#    for i in range(5):
+#        start_time = datetime.now()
+#        subprocess.Popen("./wc-plain").wait()
+#        elapsed = datetime.now() - start_time
+#        print next_test, "plain", elapsed
     subprocess.Popen("./wc-test").wait()
     for i in range(5):
         start_time = datetime.now()
