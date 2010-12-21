6d357805d87713b4c336410fafd2f7290596cb73
Chris Smowton <cs448@cam.ac.uk>
2010-12-21 16:45:35 +0000

    Used 'opt' to perform compile-time-style optimisations after linking with the syscall wrapper.
    Also converted to using a dead simple C array instead of a C++ map for the fake FD table, mainly because reading C++ type names was giving me a headache.

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ alias.bc: alias.ll
 wrappers.bc: wrappers.c
 	$(CC) $(CFLAGS) -g -std=c99 -c wrappers.c -o wrappers.bc
 
-fake_fd_table.bc: fake_fd_table.cpp
-	$(CXX) $(CXXFLAGS) -c -g fake_fd_table.cpp -o fake_fd_table.bc
+fake_fd_table.bc: fake_fd_table.c
+	$(CC) $(CFLAGS) -c -g fake_fd_table.c -o fake_fd_table.bc
 
 clean:
 	rm *.bc
--- fake_fd_table.c
+++ fake_fd_table.c
@@ -1,12 +1,13 @@
 
 #include "fake_fd_table.h"
 
-#include <map>
 #include <pthread.h>
 
 pthread_mutex_t fake_fd_mutex = PTHREAD_MUTEX_INITIALIZER;
+#define FAKE_FD_OFFSET 0x40000000
 int last_fake_fd = 0x40000000;
-std::map<int, struct fake_fd> fake_fds;
+struct fake_fd fake_fd_array[1024];
+int fake_fd_taken[1024];
 
 __attribute__((always_inline)) struct fake_fd* new_fake_fd(int* new_fd) {
 
@@ -15,7 +16,9 @@ __attribute__((always_inline)) struct fake_fd* new_fake_fd(int* new_fd) {
   pthread_mutex_lock(&fake_fd_mutex);
 
   *new_fd = last_fake_fd++;
-  ret = &fake_fds[*new_fd];
+  int offset = (*new_fd) - FAKE_FD_OFFSET;
+  ret = &fake_fd_array[offset];
+  fake_fd_taken[offset] = 1;
 
   pthread_mutex_unlock(&fake_fd_mutex);
 
@@ -29,11 +32,11 @@ __attribute__((always_inline)) struct fake_fd* get_fake_fd(int fd){
 
   pthread_mutex_lock(&fake_fd_mutex);
 
-  std::map<int, struct fake_fd>::iterator it = fake_fds.find(fd);
-  if(it != fake_fds.end())
-    ret = &(it->second);
-  else
+  int offset = fd - FAKE_FD_OFFSET;
+  if(!fake_fd_taken[offset])
     ret = 0;
+  else
+    ret = &(fake_fd_array[offset]);
 
   pthread_mutex_unlock(&fake_fd_mutex);
 
@@ -45,7 +48,7 @@ __attribute__((always_inline)) void delete_fake_fd(int fd) {
 
   pthread_mutex_lock(&fake_fd_mutex);
 
-  fake_fds.erase(fd);
+  fake_fd_taken[fd - FAKE_FD_OFFSET] = 0;
 
   pthread_mutex_unlock(&fake_fd_mutex);
 
