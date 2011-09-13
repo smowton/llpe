2db4c5d6c1cc47a4913819f3b2f9c5a875ff8282
Chris Smowton <cs448@cam.ac.uk>
2011-09-13 20:36:13 +0100

    Add a couple more tests for the loop peel heuristics

---
+++ test/nonvfs/dependent_loops.c
@@ -0,0 +1,12 @@
+
+
+int main(int argc, char** argv) {
+
+  int total = 0;
+  for(int i = 0; i < 100; i++)
+    for(int j = 0; j < 100; j++)
+      total += (i + j);
+
+  return total;
+
+}
---
+++ test/nonvfs/elim_if.c
@@ -0,0 +1,16 @@
+
+int main(int argc, char** argv) {
+
+  int total = 0;
+
+  for(int i = 0; i < 100; i++) {
+    if(i % 7 == 0)
+      total += 10;
+    if(i % 3 == 0)
+      total += 5;
+    total++;
+  }
+
+  return total;
+
+}
