b7dc8861b2c30728d6b6cc013772aa256412aec8
Chris Smowton <cs448@cam.ac.uk>
2012-11-24 20:21:56 +0000

    Add paper and diagrams

--- scripts/integrate.sh
+++ scripts/integrate.sh
@@ -2,4 +2,4 @@
 
 # Rotate loops so they can be peeled; instcombine and jump thread to eliminate the silly results of loop-rotate (e.g. trivially constant comparisons and jumps).
 # We then jump thread again because the integrator leaves lots of empty blocks lying about and we might as well automate the cleanup process.
-opt -load /home/chris/integrator/llvm/Debug+Asserts/lib/IntegratorAnalyses.so -load /home/chris/integrator/llvm/Debug+Asserts/lib/IntegratorTransforms.so -loop-rotate -instcombine -jump-threading -loopsimplify -lcssa -integrator -jump-threading $@
+opt -load /home/chris/integrator/llvm/Release/lib/IntegratorAnalyses.so -load /home/chris/integrator/llvm/Release/lib/IntegratorTransforms.so -loop-rotate -instcombine -jump-threading -loopsimplify -lcssa -integrator -jump-threading $@
